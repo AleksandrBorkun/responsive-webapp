5a3a54fc85c609e90fce3a2b146bb89496c9e0c0
Aleksandr Borkun <albo@trustpilot.com>
2020-01-03 16:00:28 +0100

    Updates

---
+++ precache-manifest.28697bc62e6eaaf3e4e45534b2cb76a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad71fbde4189f3bd7ec703efeb028ca2",
+    "url": "/responsive-webapp/index.html"
+  },
+  {
+    "revision": "021179f8e95a3548f33a",
+    "url": "/responsive-webapp/static/css/main.6f4c2db7.chunk.css"
+  },
+  {
+    "revision": "ba06e4aed52efb7a32e1",
+    "url": "/responsive-webapp/static/js/2.688a0a04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/responsive-webapp/static/js/2.688a0a04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "021179f8e95a3548f33a",
+    "url": "/responsive-webapp/static/js/main.df670b7e.chunk.js"
+  },
+  {
+    "revision": "011cac5e0b8ea71e1573",
+    "url": "/responsive-webapp/static/js/runtime-main.fbedc070.js"
+  },
+  {
+    "revision": "da5d18a3889f0c13227b59310b6a2c52",
+    "url": "/responsive-webapp/static/media/1.da5d18a3.jpg"
+  },
+  {
+    "revision": "041b234f5f48086ba26a5dce4bd1fc0f",
+    "url": "/responsive-webapp/static/media/2.041b234f.jpg"
+  },
+  {
+    "revision": "fbe68eab4723aaf9246600aa7c9abc69",
+    "url": "/responsive-webapp/static/media/3.fbe68eab.jpg"
+  },
+  {
+    "revision": "71caf797f8211c5439eb8aded8991b0b",
+    "url": "/responsive-webapp/static/media/4.71caf797.jpg"
+  }
+]);
