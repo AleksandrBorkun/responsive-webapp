4de5195a53d2f94f38ef941599fb4264c5eb375c
Aleksandr Borkun <albo@trustpilot.com>
2020-01-06 16:17:59 +0100

    update config file

--- src/configs/firebase.config.tsx
+++ src/configs/firebase.config.tsx
@@ -5,9 +5,9 @@ const config = {
     databaseURL: "https://miragame.firebaseio.com",
     projectId: "miragame",
     storageBucket: "miragame.appspot.com",
-    messagingSenderId: process.env.FB_SENDER_ID,
-    appId: process.env.FB_APP_ID,
-    measurementId: process.env.FB_MEASUREMENT_ID
+    messagingSenderId: process.env.REACT_APP_FB_SENDER_ID,
+    appId: process.env.REACT_APP_FB_APP_ID,
+    measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID
   }
 };
 
